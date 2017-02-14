b3f68b1593e659001cd76f9452be0dca616cc9a8
alejandro995 <alejodt995@gmail.com>
2017-02-13 21:41:29 -0500

    Codigo de ejercicio 1asd
    JAva adssad

---
+++ Ejercicio1.java
@@ -0,0 +1,27 @@
+import java.io.*;
+    import java.util.*;
+    import java.text.*;
+    import java.math.*;
+    import java.util.regex.*;
+
+    public class Solution {
+
+        public static void main(String[] args) {
+
+            Scanner sc=new Scanner(System.in);
+            String numerosString =sc.nextString();
+            while(numerosString.contains(" ")||numerosString.contains("-"))
+            
+            String ans="";
+            if(n%2==1){
+              ans = "Weird";
+            }
+            else{
+            
+               //Complete the code
+                
+            }
+            System.out.println(ans);
+            
+        }
+    }
