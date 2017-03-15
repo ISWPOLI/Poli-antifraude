897383201f4fb068c1ba13ed70467242f65fd2c4
alejandro995 <alejodt995@gmail.com>
2017-03-15 15:05:07 -0500

    Contruccion del Back end
    Se verifica el funcionamiento

---
+++ backend/poliantifraude/src/main/java/com/poliantifraude/PoliantifraudeApplication.java
@@ -0,0 +1,12 @@
+package com.poliantifraude;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoliantifraudeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PoliantifraudeApplication.class, args);
+	}
+}
