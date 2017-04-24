e9ce072b62f02bc1c6567269fe1c197343fd2198
alejandro995 <alejodt995@gmail.com>
2017-04-24 08:11:15 -0500

    Se creo el controlodador de los metodos de la informacion y se el modelo simulado de la base de datos del poli

---
+++ poliantifraude/src/main/java/com/poliantifraude/Controller/InfornacionController.java
@@ -0,0 +1,30 @@
+package com.poliantifraude.Controller;
+
+import com.poliantifraude.Model.Estudiante;
+import com.poliantifraude.Model.Informacion;
+import com.poliantifraude.Repo.InformationRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
+
+/**
+ * Created by alejo on 4/24/2017.
+ */
+@RestController
+public class InfornacionController {
+    @Autowired
+    InformationRepository repository;
+
+    @RequestMapping("/guardarInformacion")
+    public String generarInformacion (){
+        repository.save(new Informacion("v", "poli Bogota", "1020011670", "1015424062", "1015424062",
+                "Soltero", "Masculino", "29/06/1991", "Alejandro", "Daza", "Truque",
+                "Diagonal 182 # 20 - 72", "Bogota", "3108011284", "aldazatr"));
+
+
+        return "Se crearon los estudiantes";
+
+    }
+}
