2bae18774e6fda9fb6186083112c2835ca5a5ae2
alejandro995 <alejodt995@gmail.com>
2017-04-23 20:18:20 -0500

    Se crea modelo de la clase Pregunta

---
+++ poliantifraude/src/main/java/com/poliantifraude/Model/Pregunta.java
@@ -0,0 +1,41 @@
+package com.poliantifraude.Model;
+
+/**
+ * Created by alejo on 4/23/2017.
+ */
+public class Pregunta {
+    private String id_pregunta;
+    private String pregunta;
+    private String respuesta;
+
+
+ public void cambiarPregunta(){
+
+ }
+
+
+    public String getId_pregunta() {
+        return id_pregunta;
+    }
+
+    public void setId_pregunta(String id_pregunta) {
+        this.id_pregunta = id_pregunta;
+    }
+
+    public String getPregunta() {
+        return pregunta;
+    }
+
+    public void setPregunta(String pregunta) {
+        this.pregunta = pregunta;
+    }
+
+    public String getRespuesta() {
+        return respuesta;
+    }
+
+    public void setRespuesta(String respuesta) {
+        this.respuesta = respuesta;
+    }
+}
+
