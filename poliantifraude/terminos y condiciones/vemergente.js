dfab074bdc7e484b6683543f9ae58a447b76ab51
Andres Buitrago <buitrago001@gmail.com>
2017-05-13 14:04:47 -0500

    Se genera la actualizacion sobre el issue #65
    Se genera la actualización sobre el issue #65

--- poliantifraude/terminos y condiciones/vemergente.js
+++ poliantifraude/terminos y condiciones/vemergente.js
@@ -1,15 +1,17 @@
-<button onclick="alert('hizo click');">Click me!</button><br><br>
+<button onclick="alert('hizo click');">Click me!</button>
+<br><br>
 <input onclick = "enable();"type="checkbox" name="active_field" id="toggle">Toggle to switch
 <button id="status">Button</button>
-
 <script>
-      function enable() {
-      if (document.getElementById('toggle').checked === true) {
-      document.getElementById('status').disabled = true;
-      }
-      else 
-     {
-     document.getElementById('status').disabled = false;
-     }
-     }
+      function enable() 
+            {
+            if (document.getElementById('toggle').checked === true) 
+               {
+                  document.getElementById('status').disabled = true;
+               }
+            else
+                {
+                      document.getElementById('status').disabled = false;
+                }
+            }
 </script>
