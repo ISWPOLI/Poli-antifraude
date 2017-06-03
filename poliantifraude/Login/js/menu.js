816fb36a81325dcd1c8bb820461598f65eeea4c6
alejandro995 <alejodt995@gmail.com>
2017-06-02 21:16:05 -0500

    Se elimina
    Razon: Decision tomada por el profesor.

--- poliantifraude/Login/js/menu.js
+++
@@ -1,6 +0,0 @@
-window.onload = function(){
-	document.querySelector('.boton').addEventListener('click', function(){
-		document.querySelector('.container').classList.toogle('invisible');
-			this.classList.toogle('mif-chevron-left');
-	});
-}
